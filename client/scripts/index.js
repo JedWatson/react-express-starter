0eb2b9643914391a1256750c4bcd3a6bd57bab6f
Jed Watson <jed.watson@me.com>
2014-11-19 03:32:21 +1100

    Improving the client project structure

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,46 +1,51 @@
 var React = require('react'),
 	Router = require('react-router');
 
+var Header = React.createClass({
+	render: function() {
+		return (
+			<div className="page-header">
+				<h1>My React Project</h1>
+			</div>
+		);
+	}
+});
+
+var PageNav = React.createClass({
+	render: function() {
+		return (
+			<div className="nav">
+				<Router.Link to="home">Home</Router.Link>
+				&nbsp; | &nbsp;
+				<Router.Link to="about">About</Router.Link>
+			</div>
+		);
+	}
+});
+
 var App = React.createClass({
 	render: function() {
 		return (
 			<div className="container">
-				<div className="page-header">
-					<h1>React Express Project</h1>
-				</div>
-				<div className="nav">
-					<Router.Link to="one">Page One</Router.Link>
-					&nbsp; | &nbsp;
-					<Router.Link to="two">Page Two</Router.Link>
-				</div>
+				<Header />
+				<PageNav />
 				<this.props.activeRouteHandler/>
 			</div>
 		);
 	}
 });
 
-var One = React.createClass({
-	render: function() {
-		return (
-			<div>First page</div>
-		);
-	}
-});
-
-var Two = React.createClass({
-	render: function() {
-		return (
-			<div>Second Page</div>
-		);
-	}
-});
+var routes = {
+	Home: require('../routes/Home'),
+	About: require('../routes/About')
+};
 
 var routes = (
 	<Router.Routes location="history">
 		<Router.Route name="app" path="/" handler={App}>
-			<Router.Route name="one" path="/one" handler={One}/>
-			<Router.Route name="two" path="/two" handler={Two}/>
-			<Router.DefaultRoute handler={One}/>
+			<Router.Route name="home" path="/" handler={routes.Home}/>
+			<Router.Route name="about" path="/about" handler={routes.About}/>
+			<Router.DefaultRoute handler={routes.Home}/>
 		</Router.Route>
 	</Router.Routes>
 );
