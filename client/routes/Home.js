e70a94293bff2b6fea4c95c806915eaa8fbe2f3c
Jed Watson <jed.watson@me.com>
2015-03-31 14:20:02 +1100

    Adding link to GitHub Project

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -9,6 +9,7 @@ var Home = React.createClass({
 				<p>Browse the pages above to see examples of the included layout and form components, then dive into the code in <code>/client</code> to get started customising it.</p>
 				<p>Public assets are served from the <code>/public</code> folder, and anything .css file you include from the <code>/public/styles</code> folder will be built from the corresponding <code>.less</code> file.</p>
 				<p>The express server configuration is in <code>/server</code>. Here you can add custom server-side middleware, routes and templates, including API endpoints.</p>
+				<p><a href="https://github.com/JedWatson/react-express-starter" target="_blank">View the GitHub Project</a></p>
 			</div>
 		);
 	}
