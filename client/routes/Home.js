e1ccf82926f55309e6cb8a302c0d538bb734019f
Jed Watson <jed.watson@me.com>
2015-03-31 13:41:34 +1100

    Adding documentation to home page

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -4,9 +4,11 @@ var Home = React.createClass({
 	render: function() {
 		return (
 			<div className="page-body">
-				<h2>Home Page</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt condimentum neque, eget aliquet justo. Ut placerat euismod massa, a lacinia orci lobortis vel.</p>
-				<p>Suspendisse fringilla a neque ut efficitur. Quisque viverra felis non ullamcorper iaculis. Morbi et tincidunt augue. Vestibulum sodales bibendum convallis. Sed efficitur elit tortor, ac semper magna maximus eu. Sed mattis sem at porta varius. Donec lobortis eget nisl sit amet ultricies.</p>
+				<h2>Welcome</h2>
+				<p>This is the React + Express starter project. It includes <a href="https://facebook.github.io/react/" target="_blank">React</a> for the client, <a href="http://expressjs.com/" target="_blank">express</a> for the server, a lightweight CSS framework build with <a href="http://lesscss.org/" target="_blank">LESS</a>, <a href="https://github.com/rackt/react-router" target="_blank">react-router</a> and several UI components to get you started quickly.</p>
+				<p>Browse the pages above to see examples of the included layout and form components, then dive into the code in <code>/client</code> to get started customising it.</p>
+				<p>Public assets are served from the <code>/public</code> folder, and anything .css file you include from the <code>/public/styles</code> folder will be built from the corresponding <code>.less</code> file.</p>
+				<p>The express server configuration is in <code>/server</code>. Here you can add custom server-side middleware, routes and templates, including API endpoints.</p>
 			</div>
 		);
 	}
